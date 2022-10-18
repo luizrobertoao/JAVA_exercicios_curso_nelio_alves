4ee8f8f237f7883dcfb2fa04567879beabb13387
luizrobertoao <luizrobertoao29@gmail.com>
2022-10-18 11:27:57 -0300

    feat: Verifica se dois números inteiros são múltiplos.

--- src/com/estudos/Main.java
+++ src/com/estudos/Main.java
@@ -7,11 +7,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Integer valor;
+        double numero1;
+        double numero2;
+        double verificador;
+        int auxiliar;
 
-        System.out.println("Digite o valor:");
-        valor = sc.nextInt();
-  Valor vc = new Valor();
-  vc.verificaValor(valor);
+        System.out.println("Digite o valor para o numero1:");
+        numero1 = sc.nextInt();
+        System.out.println("Digite o valor para o numero2:");
+        numero2 = sc.nextInt();
+
+        verificador = numero1 / numero2;
+        auxiliar = (int) verificador;
+
+        if (verificador == auxiliar && verificador > 0) {
+            System.out.println("São multiplos");
+        } else {
+            System.out.println("Não são multiplos");
+        }
     }
 }
