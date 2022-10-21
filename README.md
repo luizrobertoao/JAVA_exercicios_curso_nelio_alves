62713c3bad5f68da65cd690c6b1eca3efc5579cd
luizrobertoao <luizrobertoao29@gmail.com>
2022-10-21 11:53:10 -0300

    feat: Registra alugueis de quartos e imprime relatorio de ocupacao.

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# Nome da aplicação: Filtro de pessoa mais velha.
+# Nome da aplicação: Pensionato.
 
 ## Descrição
-Programa java para criar e ler uma lista de pessoas identificando qual a mais velha.
+Programa java para criar registro de alugueis de quartos em um pensionato.
 
 ### Principais funcionalidades
-- Receber uma lista de pessoas.
-- Encontrar a pessoa mais velha da lista.
-- Mostrar o nome da pessoa mais velha.
+- Receber registro dos alugueis (nome do aluno, email e quarto escolhido).
+- Imprimir relatorio de ocupacao dos quartos.
 
 ### Quem Consome
-Usuário que quer cadastrar uma lista de pessoas, encontrar a mais velha entre elas e obter o seu nome.
+Usuário que quer registrar os alugueis de quartos de um pensionato e imprimir relatorio de ocupacao.
 
 ### Dependencias
 Este programa não possui dependencias externas
@@ -22,4 +21,4 @@ Para executar a aplicação é necessario os seguintes passos:\
 1- clonar projeto do github com o comando: **git clone** \
 2-Use esse link para o **clone**:
 `https://github.com/luizrobertoao/JAVA_exercicios_curso_nelio_alves.git` \
-3-Usar o comando para alterar a branch: git checkout feature/vetoresEx4 
+3-Usar o comando para alterar a branch: git checkout feature/desafioVetores 
--- src/com/estudos/Main.java
+++ src/com/estudos/Main.java
@@ -1,14 +1,44 @@
 package com.estudos;
 
-import com.estudos.entities.Pessoa;
+import com.estudos.entities.Aluguel;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        Aluguel[] vectAlugueis = new Aluguel[10];
+        Integer quantidadeAlugueis;
+
+        System.out.println("Quantos alunos estão alugando quartos?");
+        quantidadeAlugueis = sc.nextInt();
+
+        System.out.println("Cadastre os aluguéis:");
+
+        for (int i = 0; i < quantidadeAlugueis; i++) {
+            String nome = "";
+            String email = "";
+            Integer quartoEscolhido = 0;
+
+            System.out.print("Aluguel #" + (i + 1) + ":" + "\n" + "Nome: ");
+            sc.nextLine();
+            nome = sc.nextLine();
+            System.out.print("Email: ");
+            email = sc.nextLine();
+            System.out.print("Quarto: ");
+            quartoEscolhido = sc.nextInt();
+
+            vectAlugueis[quartoEscolhido - 1] = new Aluguel(nome, email, quartoEscolhido);
+        }
+
+        System.out.println("Quartos ocupados:");
+
+        Aluguel aluguel = new Aluguel();
+
+        aluguel.gerarRelatorioOcupacao(vectAlugueis);
+
+        /*
         Integer numeroPessoas;
 
         System.out.println("Quantas pessoas serao digitadas?");
@@ -33,6 +63,8 @@ public class Main {
         Pessoa pessoa = new Pessoa();
 
         System.out.println("A pessoa mais velha é: " + pessoa.filtrarPessoaMaisVelha(vectPessoas));
+
+         */
     }
 
 
